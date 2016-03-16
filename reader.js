0ce286e0072dce486405c33e97b2003e10e5d8e8
Samuel Steele <steelesam72@gmail.com>
2016-03-16 10:47:59 -0400

    updates 'n shit

--- reader.js
+++ reader.js
@@ -20,6 +20,30 @@ app.engine('handlebars', handlebars({
 app.set('view engine', 'handlebars')
 
 app.get('/', function(req, res) {
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            res.status(522).render('index', {
+                endpoint: "Samuel Steele",
+                error: Error(522, req.path)
+            })
+        } else {
+            db.collection('posts').find().sort({
+                "date.created": 1
+            }).limit(10).toArray(function(err, posts) {
+                if (err) {
+                    res.status(578).render('index', {
+                        endpoint: "Samuel Steele",
+                        error: Error(578, req.path)
+                    })
+                } else {
+                    res.render('index', {
+                        endpoint: "Samuel Steele",
+                        posts: posts
+                    })
+                }
+            })
+        }
+    })
     res.render('index', {
         endpoint: 'Samuel Steele',
         posts: '',
@@ -32,11 +56,7 @@ app.get('/post/:id', function(req, res) {
         if (err) {
             res.status(522).render('post', {
                 endpoint: 'Post',
-                error: {
-                    code: 522,
-                    label: "No Connection",
-                    body: "Unable to establish connection to database"
-                }
+                error: Error(522, req.path)
             })
             console.error("Unable to connect to MongoDB: ", err)
         } else {
@@ -46,11 +66,7 @@ app.get('/post/:id', function(req, res) {
                 if (err) {
                     res.status(578).render('post', {
                         endpoint: 'Post',
-                        error: {
-                            code: 578,
-                            label: "Database Search Error",
-                            body: "Unable to search the database"
-                        }
+                        error: Error(578, req.path)
                     })
                 } else {
                     res.render('post', {
@@ -78,11 +94,7 @@ app.get('/api/v1/posts', function(req, res) {
         } else {
             db.collection('posts').find().toArray(function(err, posts) {
                 if (err) {
-                    res.status(578).json({
-                        code: 578,
-                        label: "Database Search Error",
-                        body: "Unable to search the database"
-                    })
+                    res.status(578).json(Error(578, req.path))
                 } else {
                     res.json(posts)
                 }
@@ -94,25 +106,14 @@ app.get('/api/v1/posts', function(req, res) {
 app.get('/api/v1/post/:id', function(req, res) {
     MongoClient.connect(url, function(err, db) {
         if (err) {
-            res.status(522).json({
-                success: false,
-                error: {
-                    code: 522,
-                    label: "No Connection",
-                    body: "Unable to establish connection to database"
-                }
-            })
+            res.status(522).json(Error(522, req.path))
             console.error("Unable to connect to MongoDB: ", err)
         } else {
             db.collection('posts').findOne({
                 _id: new ObjectID(req.params.id)
             }, function(err, post) {
                 if (err) {
-                    res.status(578).json({
-                        code: 578,
-                        label: "Database Search Error",
-                        body: "Unable to search the database"
-                    })
+                    res.status(578).json(Error(578, req.path))
                     console.error("Unable to search MongoDB: ", err)
                 } else {
                     res.json(post)
@@ -122,6 +123,40 @@ app.get('/api/v1/post/:id', function(req, res) {
     })
 })
 
+function Error(code, endpoint) {
+    switch (code) {
+        case 522:
+            return {
+                endpoint: endpoint,
+                success: false,
+                error: {
+                    code: 522,
+                    label: "No Connection",
+                    body: "Unable to establish connection to database"
+                }
+            }
+        case 578:
+            return {
+                endpoint: endpoint,
+                success: false,
+                error: {
+                    code: 578,
+                    label: "Database Error",
+                    body: "Unable to query the database"
+                }
+            }
+        default:
+            return {
+                endpoint: endpoint,
+                success: false,
+                error: {
+                    code: 500,
+                    label: "Internal Server Error",
+                    body: "Internal Server Error"
+                }
+            }
+    }
+}
 
 var server = app.listen(process.env.PORT || 5000, function() {
     console.log("Listening at http://0.0.0.0:%d", server.address().port)
